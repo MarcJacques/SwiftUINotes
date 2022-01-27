abb3130623a7c36b5f5454e6d08e95b6a7fca32d
Marc <marcjacques@me.com>
2022-01-26 20:29:40 -0500

    Add Views define their dependencies

--- SwiftUINotes/SwiftUINotes.playground/Contents.swift
+++ SwiftUINotes/SwiftUINotes.playground/Contents.swift
@@ -9,7 +9,12 @@ var greeting = "Hello, playground"
             - apple encourages to make as many views as possible.
             - views can be created and destroyed very simply
  2. Views define a  piece of UI
-            - all views have a body
-            - everything inside the body are viewBuilders
-                - in the view builder you can modify size, color, font and many other aspects of the view
+        - all views have a body
+        - everything inside the body are viewBuilders
+            - in the view builder you can modify size, color, font and many other aspects of the view
+ 3. Views define their dependencies
+        - @State  properties are a source of truth
+            - your telling swiftUI that based off the property of that state adjust the view.
+            - the system must monitor any changes to that property to make adjustments to the view.
+        
  */
