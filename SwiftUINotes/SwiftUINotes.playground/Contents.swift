73958e30d7cf1d4de55ccb398b77919b95fbe2b9
Marc <marcjacques@me.com>
2022-01-26 20:23:09 -0500

    Updated files

---
+++ SwiftUINotes/SwiftUINotes.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+/*
+ 1. SwiftUI uses structs and they  inherit from the View Class
+        views are value types in swiftUI hence the reason it uses structs
+            - apple encourages to make as many views as possible.
+            - views can be created and destroyed very simply
+ 2. Views define a  piece of UI
+            - all views have a body
+            - everything inside the body are viewBuilders
+                - in the view builder you can modify size, color, font and many other aspects of the view
+ */
